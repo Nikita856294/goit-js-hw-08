a6886dd5b07ff291119e8117f579ce7c14c6cfd2
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-10-30 11:45:45 +0300

    сделал 2 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframeRef);
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+// console.log(player);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function setTime() {
+  const seconds = localStorage.getItem('videoplayer-current-time');
+  if (seconds) {
+    player.setCurrentTime(seconds);
+  }
+}
+window.addEventListener('DOMContentLoaded', setTime);
