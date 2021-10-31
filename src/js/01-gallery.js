4c047407301373f5a756b3cee2af9c57bbc3f36d
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-10-31 19:37:57 +0200

    сделал 1 задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
 
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return ` 
+      <li>
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+    </a>
+    </li>`;
+    })
+    .join('');
+}
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox');
 console.log(galleryItems);
