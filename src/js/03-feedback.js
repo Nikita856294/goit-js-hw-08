f64f82a967768604422074ceb6b54ab1d3379ca2
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-10-31 23:44:02 +0200

    сделал 3 задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const formRef = document.querySelector('.feedback-form');
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+const data = {
+  email: '',
+  message: '',
+};
+window.addEventListener('DOMContentLoaded', initForm);
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(formRef);
+  formData.forEach((value, name) => (formData[name] = value));
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM_STATE);
+});
 
+formRef.addEventListener('input', e => {
+  data[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(data));
+});
+
+function initForm() {
+  let dataLocalStorage = localStorage.getItem(FEEDBACK_FORM_STATE);
+  if (dataLocalStorage) {
+    dataLocalStorage = JSON.parse(dataLocalStorage);
+    data.email = dataLocalStorage.email;
+    data.message = dataLocalStorage.message;
+    formRef.elements.email.value = dataLocalStorage.email;
+    formRef.elements.message.value = dataLocalStorage.message;
+  }
+}
